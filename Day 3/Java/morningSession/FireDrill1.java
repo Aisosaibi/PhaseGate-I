f3d669c34eab698216629ed689a3c303d4cafe30
Aisosaibi <Aisosaibi.w@gmail.com>
2026-05-20 12:29:20 +0100

    a test on testing

---
+++ Day 3/Java/morningSession/FireDrill1.java
@@ -0,0 +1,48 @@
+public class FireDrill1{
+    public static void main(String[] args){
+        String[] usersA;
+        String[] usersB;
+        String[] usersC;
+        String[] usersD;
+        String[] usersE;
+        String[] usersF;
+//        System.out.println(facebookLikes(usersA));
+    }
+
+    public static int numberOfUsers(String... users){
+        int count = users.length;
+        int userCount = 0;
+
+        for (int i = 0; i < count; i++){
+            if (users[i] == null) continue;
+            userCount++;
+        }
+        return userCount;
+    }
+
+    public static String[] usernames(String... users){
+        int count = numberOfUsers(users);
+        String[] usernames = new String[count];
+
+        if (count > 0) {
+            for (int i = 0; i < count; i++){
+                if (users[i] == null) continue;
+                usernames[i] = users[i];
+            }
+        }
+        return usernames;
+    }
+
+    public static void facebookLikes(String... users){
+        int count = numberOfUsers(users);
+        String[] user = usernames(users);
+        
+        switch (count){
+            case 0: System.out.println("no one likes this"); break;
+            case 1: System.out.printf("%s likes this", users[0]); break;
+            case 2: System.out.printf("%s and %s likes this", users[0], users[1]); break;
+            case 3: System.out.printf("%s, %s and %s like this", users[0], users[1], users[2]); break;
+            default: System.out.printf("%s, %s and %d others like this", user[0], user[1], (count - 2)); break;
+        }
+    }
+}
---
+++ Day 3/Java/morningSession/FireDrill1Test.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FireDrill1Test{
+
+    @Test
+    public void testThatnumberOfUsersReturnsTwoUsers(){
+        int expected = 2;
+        String[] users = {"Ahmed", "Musa"};
+        int actual = numberOfUsers(users);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testThatnumberOfUsersReturnsZeroUsers(){
+        int expected = 0;
+        String[] users = new String[5];
+        int actual = numberOfUsers(users);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testThatusernamesReturnsTwoUsernames(){
+        String[] expected = {"Ahmed", "Musa"};
+        String[] users = {"Ahmed", "Musa", null};
+        String actual = usernames(users);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testThatusernamesReturnsNullUsernames(){
+        String[] expected = null;
+        String[] users = new String[5];
+        String actual = usernames(users);
+        assertArrayEquals(expected, actual);
+    }
+
+}
